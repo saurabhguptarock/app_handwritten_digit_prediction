445426ccdcfd46a7231e56545742a4337b0fc52a
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-08-05 23:35:48 +0530

    demo

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ from keras.models import load_model
 app = Flask(__name__)
 
 
-@app.route('/<url>')
+@app.route('/<url>', methods=['POST'])
 def index(url):
     # model = load_model('model.h5')
     return url
